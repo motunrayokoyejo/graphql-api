3efa492fed0db75899769ece721057ae566e7ce4
Motunrayo Koyejo <koyejomotunrayo@gmail.com>
2020-11-06 23:39:33 +0300

    made adjustment

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const gql = require('graphql-tag')
 const { buildSchema } = require('graphql')
+const fetch = require('node-fetch');
 // const schema = require('./schema')
 // const rootValue = require('./rootvalue')
 
 const app = express()
 const schema = buildSchema(`
 type Query {
-    calculatePrice (type: String!, margin: Float!,exchangeRate :Int! ) : String
+    calculatePrice (type: String!, margin: Float!,exchangeRate :Int! ) : Int
 }
 `)
 const rootValue = {
     calculatePrice : (type,margin,exchangeRate) => {
-
-        return 'This is working'
+  fetch("https://api.coindesk.com/v1/bpi/currentprice/USD.json")
+  .then(res => res.json())
+  .then(json => {
+      const btcRate = json.bpi.USD.rate_float
+      const marginPercent = margin * btcRate
+      console.log({rate: btcRate,
+        margin: margin})
+      if(type.toString().toLowerCase() === "buy") {
+       const computedValue = btcRate + marginPercent
+       return computedValue/exchangeRate
+      }
+      else if (type.toString().toLowerCase() === "sell") {
+          const computedValue = btcRate - marginPercent
+          return computedValue/exchangeRate
+      }
+      console.log(json.bpi.USD)
+  })
+  .catch(err => console.log(err))
     }
   }
 
